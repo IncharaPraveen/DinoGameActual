0a466f0b1588d515fa02ebc1ef373f85596c15a1
IncharaPraveen <125172215+IncharaPraveen@users.noreply.github.com>
2025-01-10 10:29:13 +0000

    made changes

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -19,21 +18,45 @@ const config = {
 };
 
 
+
+
 const game = new Phaser.Game(config);
 
 
-let player;
-let clouds;
-let ground;
-let obstacles;
-let gameOverText;
-let restartText;
-let gameOverContainer;
+
+
+
+let player;                // The player sprite (Dino)
+let score;                  // The current score of the game
+let clouds;                 // Group for the cloud objects
+let ground;                 // The ground sprite used in the game
+let obstacles;              // Group for the obstacle objects (cacti)
+let scoreText;              // Text object displaying the current score
+let gameOverText;           // Image object for the "Game Over" screen
+let restartText;            
+let gameOverContainer;  
+let highScore = localStorage.getItem('highScore') ? parseInt(localStorage.getItem('highScore')) : 0; // Retrieve the saved high score
 function gameOver(){
+   /* const newHighScore = this.highScoreText.text.substring(this.highScoreText.text.length - 5);
+    const newScore = Number(this.scoreText.text) > Number(newHighScore)
+     ? this.scoreText.text: newHighScore;
+     this.highScoreText.setText("HI" +newScore);
+     this.highScoreText.setAlpha(1); */
+   
+    if (this.score > this.highScore) {
+        this.highScore = this.score; // Update high score
+        localStorage.setItem('highScore', highScore);
+        this.highScoreText.setText("HI" + String(this.highScore).padStart(4, '0')); // Update high score text
+    }
+    this.highScoreText.setAlpha(1);
+    this.gameOverContainer.setAlpha(1);
     this.isGameRunning = false
     this.physics.pause();
-    this.gameOverContainer.setAlpha(1);
+   
+   
  }
+
+
 
 
 function preload() {
@@ -47,11 +70,13 @@ function preload() {
     this.load.image("restart", "assets/restart.png");
 
 
-    for(let i =0; i<6 ; i++){
+    for (let i = 0; i < 6; i++) {
         const cactusNum = i + 1;
-        this.load.image(`obstacle-${cactusNum}`,`assets/cactuses_${cactusNum}.png`) ;
-   }
+        this.load.image(`obstacle-${cactusNum}`, `assets/cactuses_${cactusNum}.png`); // Corrected template literal
+    }
 }
+
+
 function create() {
     this.isGameRunning = true;
     this.timer = 0;
@@ -64,11 +89,15 @@ function create() {
     .setDepth(1);
 
 
+
+
     this.ground=this.add.tileSprite(0, 300, 1000, 30, "ground")
     .setOrigin(0,1);
      this.physics.world.enable(this.ground);  // Enable physics on the ground
     this.ground.body.setAllowGravity(false);  // Prevent ground from being affected by gravity
     this.ground.body.setImmovable(true);  // Make sure the ground doesn't move
+
+
 
 
     // Add collision between the player and the ground
@@ -81,11 +110,8 @@ function create() {
    
 ])
 this.obstacles = this.physics.add.group()
+this.physics.add.collider(this.obstacles, this.player, gameOver,null,this);
 
-
-
-
- this.physics.add.collider(this.obstacles, this.player, gameOver,null,this);
 
 this.gameOverText = this.add.image(0,0, "game-over");
 this.restartText= this.add.image(0,80,"restart").setInteractive();
@@ -96,12 +122,57 @@ this.gameOverContainer = this.add
 .setAlpha(0);
 
 
+this.scoreText = this.add.text(700,30, "0000", {
+    fontSize : 30,
+    fontFamily : "Arial",
+    color : "535353",
+    resolution : 5
+}).setOrigin(1,0);
+this.highScoreText = this.add.text(this.scoreText.getBounds().left - 20,0 ,"0000", {
+    fontSize : 30,
+    fontFamily : "Arial",
+    color : "535353",
+    resolution : 5
+}).setOrigin(1,0).setAlpha(0);
+
+
+this.score = 0;
+this.frameCounter = 0;
+this.gameSpeed = 13;
+
+
+this.restartText.on("pointerdown", () =>{
+    this.physics.resume();
+    this.player.setVelocityY(0);
+    this.obstacles.clear(true,true);
+   
+    this.frameCounter = 0;
+   this.score = 0; // Reset the score to 0
+    const formattedScore = String(Math.floor(this.score));
+    this.scoreText.setText(formattedScore);
+    this.isGameRunning = true;
+    this.gameOverContainer.setAlpha(0);
+   
+   
+     })
+
+
 }
 
 
+
+
 function update(time,delta) {
-    if(!this.isGameRunning)
-    return;
+    if(!this.isGameRunning){
+        return;}
+    this.frameCounter ++;
+    if(this.frameCounter%100 == 0){
+        this.score += 100;
+      const formattedScore = String (Math.floor(this.score));
+      this.scoreText.setText(formattedScore);
+    }
+
+
      this.cursors = this.input.keyboard.createCursorKeys();
      const {space,up} = this.cursors;
     const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space)
@@ -109,19 +180,20 @@ function update(time,delta) {
      const onFloor = this.player.body.onFloor();
     if(isSpaceJustDown && onFloor){
         this.player.setVelocityY(-1600);
+   
     }
  
 this.ground.tilePositionX += this.gameSpeed;
 Phaser.Actions.IncX(this.obstacles.getChildren(), -this.gameSpeed);
 this.timer += delta;
 console.log(this.timer);
+
+
 while(this.timer > 1000) {
  this.obstacleNum = Math.floor((Math.random()*6)+1);
 this.obstacle = this.obstacles.create(700,200, `obstacle-${this.obstacleNum}`).setOrigin(0);
 this.obstacle.body.allowGravity = false;
 this.timer -= 1000;
-
-
 }
 //regular creation of random obstacles
   this.obstacles.getChildren().forEach(obstacle => {
@@ -131,5 +203,16 @@ this.timer -= 1000;
       }
      //obstacle deleted once leaving frame
      });
- 
+
+
 }
+//to make highscore, create highscore variable, then in create, store all scores in an array before update ends
+//then, in update, compare every score to the array elements, if its more, print it !
+
+
+
+
+
+
+
+
